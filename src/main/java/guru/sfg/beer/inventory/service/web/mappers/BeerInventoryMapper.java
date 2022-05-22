6e23956b8befececb42d9d5048280eae20df9030
Nicu Taban <nictbndeveloper@gmail.com>
2022-05-22 21:55:24 +0300

    Handles Allocation Requests. Closes #13

--- src/main/java/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
+++ src/main/java/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapper.java
@@ -1,7 +1,7 @@
 package guru.sfg.beer.inventory.service.web.mappers;
 
 import guru.sfg.beer.inventory.service.domain.BeerInventory;
-import guru.sfg.brewery.model.events.BeerInventoryDto;
+import guru.sfg.brewery.model.BeerInventoryDto;
 import org.mapstruct.Mapper;
 
 /**
